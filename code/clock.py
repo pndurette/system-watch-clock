54a4235925f3c4e1039bc7b6d5afa60d3163a9a7
Pierre Nicolas Durette <pndurette@gmail.com>
2023-03-12 18:07:20 -0700

    feat: add Clock, remove offset from ClockHand

--- code/clock.py
+++ code/clock.py
@@ -1,7 +1,7 @@
 from math import cos, radians, sin
 
 from displayio import Group
-from vectorio import Polygon
+from vectorio import Circle, Polygon, Rectangle
 
 
 class ClockHand(Group):
@@ -24,11 +24,9 @@ class ClockHand(Group):
 
                    +------length-----+
                    |                 | width
-               - - + - * (x,y) - - - + - - - angle
+               - - * (x,y) - - - - - + - - - angle
                    |                 |         v
                    +-----------------+
-                   |<->|
-                    width/2
 
         Args:
             x: The x-axis coordinate of the rotation point
@@ -66,17 +64,15 @@ class ClockHand(Group):
         Calculate the 4-point initial coordinates of the clock hand
 
         The rectangle with its centre of rotation at (0,0)
-        which is an offset of half the width from the centre-left
-        of the shape:
+        which is on centre-left edge of the shape:
 
            (x0,y0)                    (x1,y1)
                    +------length-----+
                    |                 |
-                   |   * (0,0)       width
+                   * (0,0)           width
                    |                 |
                    +-----------------+
-           (x3,y3) |<->|              (x2,y2)
-                    width/2
+           (x3,y3)                   (x2,y2)
 
         Args:
             length: The lenght of the hand, in pixels
@@ -85,10 +81,10 @@ class ClockHand(Group):
         Returns:
             The list of points (tuples) to create the rectangle
         """
-        x0, y0 = 0 - width / 2,          0 + width / 2  # fmt: skip
-        x1, y1 = 0 - width / 2 + length, 0 + width / 2
-        x2, y2 = 0 - width / 2 + length, 0 - width / 2
-        x3, y3 = 0 - width / 2,          0 - width / 2  # fmt: skip
+        x0, y0 = 0,          0 + width / 2  # fmt: skip
+        x1, y1 = 0 + length, 0 + width / 2
+        x2, y2 = 0 + length, 0 - width / 2
+        x3, y3 = 0,          0 - width / 2  # fmt: skip
 
         # Points as vectorio.Polygon expects
         # i.e (List[Tuple[int,int]])
@@ -128,3 +124,132 @@ class ClockHand(Group):
 
         # Group (self) only has the Polygon, refresh its points
         self[0].points = new_points
+
+    @property
+    def points(self):
+        # Group (self) only has the Polygon, get its points
+        return self[0].points
+
+
+class Clock(Group):
+    def __init__(
+        self,
+        x: int,
+        y: int,
+        hours: int,
+        minutes: int,
+        pixel_shader,
+        color_index: int = 0,
+    ):
+        """
+        A minimalist clock
+
+        Two ClockHands in a displayio Group representing to display time
+        plus an endcap (vectorio.Polygon) to join the two ClockHands
+        neatly in their centre.
+
+        Args:
+            x: The x-axis coordinate of the rotation point of the clock
+            y: The y-axis coordinate of the rotation point of the clock
+            hours: The initial time (hours) to show
+            minutes: The initial time (minutes) to show
+            pixel_shader: The pixel_shader, as expected by vectorio
+            color_index: The color_index, as expected by vectorio
+        """
+        super().__init__()
+        self._minutes = minutes
+        self._hours = hours
+
+        # Create ClockHand for the hours
+        self._hours_hand = ClockHand(
+            x=x,
+            y=y,
+            length=20 * 3,
+            width=20,
+            angle=self._hours_to_degrees(hours),
+            pixel_shader=pixel_shader,
+            color_index=color_index,
+        )
+
+        # Create ClockHand for the minutes
+        self._minutes_hand = ClockHand(
+            x=x,
+            y=y,
+            length=20 * 4,
+            width=20,
+            angle=self._minutes_to_degrees(minutes),
+            pixel_shader=pixel_shader,
+            color_index=color_index,
+        )
+
+        # Create an endcap Polygon to join
+        # the hands neatly in the middle
+        _endcap_points = self._calculate_endcap_points(
+            self._hours_hand.points, self._minutes_hand.points
+        )
+        self._endcap = Polygon(
+            pixel_shader=pixel_shader, points=_endcap_points, x=x, y=y
+        )
+
+        # TODO: Manage the indices better
+        self.append(self._hours_hand)  # 0
+        self.append(self._minutes_hand)  # 1
+        self.append(self._endcap)  # 2
+
+    def _calculate_endcap_points(self, hours_hand_points, minutes_hand_points):
+        """
+        Calculate the 4-point coordinates of the shape joining the centre
+        endpoints of the two hands (i.e. (x0,y0) and (x3,y3) of each)
+
+        Args:
+            hours_hand_points: List of point tuples of the hours hand shape
+            minutes_hand_points: List of point tuples of the minutes hand shape
+
+        Returns:
+            List of point tuples to display a Polygon to fill the gap
+        """
+        hours_p0, _, _, hours_p3 = hours_hand_points
+        min_p0, _, _, min_p3 = minutes_hand_points
+        return [hours_p0, min_p0, hours_p3, min_p3]
+
+    def _minutes_to_degrees(self, minutes: int):
+        # 6 degrees per minute (i.e 360/60)
+        # Minus 90 degrees offset (0 degrees is on the x-axis)
+        return minutes * 6 - 90
+
+    def _hours_to_degrees(self, hours: int):
+        # 30 degrees per hour (i.e 360/12)
+        # Minus 90 degrees offset (0 degrees is on the x-axis)
+        return hours * 30 - 90
+
+    @property
+    def hours(self):
+        return self.hours
+
+    @hours.setter
+    def hours(self, new_hours: int):
+        # Update hours
+        self._hours = new_hours
+        print(f"debug: {self} (hours) {new_hours}")
+
+        # Update hours clockhand
+        self[0].angle = self._hours_to_degrees(new_hours)
+
+        # Update endcap
+        self[2].points = self._calculate_endcap_points(self[0].points, self[1].points)
+
+    @property
+    def minutes(self):
+        return self._minutes
+
+    @minutes.setter
+    def minutes(self, new_minutes: int):
+        # Update minutes
+        self._minutes = new_minutes
+        print(f"debug: {self} (minutes) {new_minutes}")
+
+        # Update minutes clockhand
+        self[1].angle = self._minutes_to_degrees(new_minutes)
+
+        # Update endcap
+        self[2].points = self._calculate_endcap_points(self[0].points, self[1].points)
--- code/code.py
+++ code/code.py
@@ -1,19 +1,13 @@
 import time
+
 import board
 import displayio
-import vectorio
-from math import sin, cos, pi
 
-from clock import ClockHand
+from clock import Clock
+from utils import lights
 
-# Add a light to see (on the Magtag)
-import neopixel
-
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 4, brightness=0.1)
-pixels[0] = (255, 255, 255)
-pixels[1] = (255, 255, 255)
-pixels[2] = (255, 255, 255)
-pixels[3] = (255, 255, 255)
+# Turn on some lights during dev!
+lights(True)
 
 # "Pixel" size (for the hands) in real pixels
 pixel_size = 20
@@ -28,29 +22,28 @@ display.rotation = 0
 main_group = displayio.Group()
 display.show(main_group)
 
-# Make a background color fill
-color_bitmap = displayio.Bitmap(display.width, display.height, 1)
-color_palette = displayio.Palette(1)
-color_palette[0] = 0xFFFFFF  # White
-bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
+# Background color fill
+bg_bitmap = displayio.Bitmap(display.width, display.height, 1)
+bg_palette = displayio.Palette(1)
+bg_palette[0] = 0xFFFFFF  # White
+bg_sprite = displayio.TileGrid(bg_bitmap, pixel_shader=bg_palette, x=0, y=0)
 main_group.append(bg_sprite)
 
-# Fill Colour
-_palette = displayio.Palette(1)
-_palette[0] = 0x0  # Black
+# Clock fill
+clock_palette = displayio.Palette(1)
+clock_palette[0] = 0x0  # Black
 
-hand = ClockHand(
+# The clock
+clock = Clock(
     x=int(display.width / 2),
     y=int(display.height / 2),
-    length=pixel_size * 3,
-    width=pixel_size,
-    angle=0,  # HOUR_RAD * 12 - OFFSET_RAD,
-    pixel_shader=_palette,
+    hours=12,
+    minutes=15,
+    pixel_shader=clock_palette,
 )
-main_group.append(hand)
 
-hand.angle = 45
-hand.angle = 90
+# Add to group
+main_group.append(clock)
 
 # Do refresh after it's okay for the ePaper (plus an arbitrary wait)
 time.sleep(display.time_to_refresh + 0.2)
