7f420dbba198196c4c7af519d74b75d0dbd6e025
Pierre Nicolas Durette <pndurette@gmail.com>
2023-03-12 18:20:59 -0700

    fix: use lights() properly

--- code/utils.py
+++ code/utils.py
@@ -1,14 +1,15 @@
-def lights(on=True):
-    # Turn on some lights during dev!
-    # (Magtag has 4 neopixels)
-    if on:
-        import board
-        import neopixel
+import neopixel
+import board
 
-        pixels = neopixel.NeoPixel(board.NEOPIXEL, 4, brightness=0.1)
-        pixels[0] = (255, 255, 255)
-        pixels[1] = (255, 255, 255)
-        pixels[2] = (255, 255, 255)
-        pixels[3] = (255, 255, 255)
+PIXEL_COUNT = 4  # MagTag
+PIXELS = neopixel.NeoPixel(board.NEOPIXEL, PIXEL_COUNT, brightness=0.1)
+
+
+def lights(on: bool):
+    # Turn on some lights during dev!
+    if on:
+        for p in range(0, PIXEL_COUNT):
+            PIXELS[p] = (255, 255, 255)  # White
     else:
-        pixels = neopixel.NeoPixel(board.NEOPIXEL, 4, brightness=0)
+        for p in range(0, PIXEL_COUNT):
+            PIXELS[p] = (0, 0, 0)  # Off
